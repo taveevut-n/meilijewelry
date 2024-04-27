41678c0b0af0a1979aa0bafba9897d3978db7730
Taveevut <nakomah.web@gmail.com>
2024-04-27 13:15:31 +0700

    fix (categories): split file as components and include

---
+++ components/categories.php
@@ -0,0 +1,37 @@
+<table width="300" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=1"><span style="color:#000">ขายส่งเพชรสวิส พลอยสีต่าง ๆ / 水晶钻石 宝石等</span></a>
+    </td>
+  </tr>
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=2"><span style="color:#000">แหวน / 戒子</span></a>
+    </td>
+  </tr>
+  <!-- <tr>
+        <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+          <a href="show_product.php?cat_id=3"><span style="color:#000">กำไล - สร้อยพระทองต่างๆ</span></a>
+        </td>
+      </tr> -->
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=4"><span style="color:#000">กัานปู แหนบ จี้ กำไล - สร้อย / 活扣-夹子-垂饰-手镯-项链</span></a>
+    </td>
+  </tr>
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=5"><span style="color:#000">กรอบเงิน เครื่องเงิน / 银壳 银首饰</span></a>
+    </td>
+  </tr>
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=3"><span style="color:#000">กรอบ-ตลับทอง - งานทอง / 金壳开关壳 金首饰</span></a>
+    </td>
+  </tr>
+  <tr>
+    <td height="45" align="center" valign="top" style="background:url(images/bg-catalog.png) no-repeat; padding-top:10px">
+      <a href="show_product.php?cat_id=6"><span style="color:#000">อื่นๆ / 其它</span></a>
+    </td>
+  </tr>
+</table>
